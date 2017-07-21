fe12e5d0634e8c816d488223ad21713e282d28cb
Stephan Michard <stephan.michard@gmail.com>
2017-07-21 11:13:21 +0200

    small adjustments

--- graphr/host_summary.R
+++ graphr/host_summary.R
@@ -2,6 +2,10 @@ source("libraries.R")
 
 overview_host <- data.frame(read_excel("export.xlsx", sheet="tabvHost", col_names=TRUE))
 
+overview_host <- data.frame(read_excel(choose.files(), sheet="tabvHost", col_names=TRUE))
+
+
+exists("overview_host")
 host_sub <- overview_host[, c("Host", "Datacenter", "CPU.Model", "X..CPU", "Cores.per.CPU", "X..Cores", "X..Memory", "X..vCPUs", "ESX.Version")]
 
 colnames(host_sub) <- c("Host", "Datacenter", "CPU_Model", "n_CPU", "Cores_per_CPU", "n_Cores", "Memory", "n_vCPU", "ESX_Version")
