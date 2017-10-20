f0aafd5ce818d19df72b98f607e1e3570d872bbe
Stephan Michard <stephan.michard@gmail.com>
2017-10-21 01:01:38 +0200

    version branch created

--- set-pipeline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]
-  then
-    fly -t lite set-pipeline -p graphr_app -c ci/pipeline.yml -l ci/credentials.yml
-  else
-    fly -t $1 set-pipeline -p graphr_app -c ci/pipeline.yml -l ci/credentials.yml
-fi
